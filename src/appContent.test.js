6dbd45440b4f8cddd6e66383db5a55ecf578b4ad
sashida <sashida@primagest.co.jp>
2020-10-30 18:33:24 +0900

    p.504

--- src/appContent.test.js
+++ src/appContent.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import App from "./App";
 import { ValueInput } from "./ValueInput";
 
@@ -10,4 +10,16 @@ it("Renders three Value Inputs", () => {
     const wrapper = shallow(<App />);
     const valCount = wrapper.find(ValueInput).length;
     expect(valCount).toBe(3);
-})
+});
+
+it("Fully renders three inputs", () => {
+    const wrapper = mount(<App title="tester" />);
+    const count = wrapper.find("input.form-control").length;
+    expect(count).toBe(3);
+});
+
+it("Shallow renders zero inputs", () => {
+    const wrapper = shallow(<App />);
+    const count = wrapper.find("input.form-control").length;
+    expect(count).toBe(0);
+});
