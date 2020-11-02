fc3921b123746327f0eac9c682875cee50555ee6
sashida <sashida@primagest.co.jp>
2020-11-02 09:15:48 +0900

    p.509

--- src/appBehavior.test.js
+++ src/appBehavior.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import App from './App';
+import { ValueInput } from "./ValueInput";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -40,4 +41,16 @@ it("updates total when button is clicked", () => {
     button.simulate("click");
 
     expect(wrapper.state("total")).toBe(values.reduce((total, val) => total + val), 0);
+});
+
+it("child function prop updates state", () => {
+    const wrapper = mount(<App />);
+    const valInput = wrapper.find(ValueInput).first();
+    const inputElem = valInput.find("input").first();
+
+    inputElem.simulate("change", { target: { value: "100" } });
+    wrapper.instance().updateTotal();
+
+    expect(valInput.state("fieldValue")).toBe("100");
+    expect(wrapper.state("total")).toBe(100);
 });
